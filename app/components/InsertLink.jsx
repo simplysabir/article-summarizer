20f9498f3a10d81dc20616393ff29c7c8e7f9360
simplysabir <simplysabir@gmail.com>
2023-06-13 20:22:40 +0530

    ⚒️fix : Insert Url Component Updated

--- app/components/InsertLink.jsx
+++ app/components/InsertLink.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const InsertLink = () => {
   return (
-    <div>InsertLink</div>
+    <section className='font-mono flex items-center flex-col insert-link'>
+        <div>
+            <h1 className='insert-heading font-mono'>Insert Link</h1>
+        </div>
+        <div className='font-mono'>
+            <input type="text" name="url" id="url" placeholder='insert url ..' className='px-6 py-1' />
+        </div>
+    </section>
   )
 }
 
