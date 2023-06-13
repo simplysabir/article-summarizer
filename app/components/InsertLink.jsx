969da08551deb12fc21747bdd6faed6876e3528a
simplysabir <simplysabir@gmail.com>
2023-06-13 20:02:42 +0530

    ✨ feat : Inserting Link Section Added

---
+++ app/components/InsertLink.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InsertLink = () => {
+  return (
+    <div>InsertLink</div>
+  )
+}
+
+export default InsertLink
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import InsertLink from './components/InsertLink'
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <Hero />
+    <InsertLink />
     </>
   )
 }
